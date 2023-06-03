f788b42cc3ab21746fc0ed0540bddf38d83010df
hjs <legoesprit@kabelbw.de>
2023-06-03 13:37:57 +0200

    Initial Commit

---
+++ TestLineCounters/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  TestLineCounters
+//
+//  Created by Heinz-Jörg on 03.06.23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ TestLineCounters/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestLineCounters
+//
+//  Created by Heinz-Jörg on 03.06.23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
