5f1477ba10502f0af7ae8dd86bef40ebe603ff0b
hjs <legoesprit@kabelbw.de>
2023-06-04 09:44:48 +0200

    First version even for lineNumber 2 working

--- TestLineCounters/ViewController.swift
+++ TestLineCounters/ViewController.swift
@@ -9,16 +9,33 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet var arrayController: NSArrayController!
+    @IBOutlet var textView: LoremIpsumTextView!
+    
+    @IBOutlet weak var label1: NSTextField!
+    @IBOutlet weak var label2: NSTextField!
+    @IBOutlet weak var label3: NSTextField!
+    @IBOutlet weak var label4: NSTextField!
+    @IBOutlet weak var label5: NSTextField!
+    @IBOutlet weak var labelT: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        textView.viewController = self
+        populateTable()
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    func populateTable() {
+        let table = ["Id": 0, "Name": "Test", "Address": "VS"] as [String : Any]
+        arrayController.addObject(table)
+        tableView.reloadData()
     }
 
 
